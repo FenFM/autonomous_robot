fa6db601dd8ea2870460cf42cfd42926caf2b6e3
FenFr <HohesCAlt@gmail.com>
2023-05-29 22:28:47 +0200

    Improved the testfile

--- testfiles/rbs.c
+++ testfiles/rbs.c
@@ -3,8 +3,8 @@
 //  Lecturer:               Prof. Dr.-Ing. habil. Ralf Salomon              //
 //  Author:                 B.Sc. Fenja Freitag                             //
 //  Name:                   rbs.c                                           //
-//  Description:            simple rbs neuronal-net                         //
-//  Version:                0.1                                             //
+//  Description:            simple rbs neuronal net test                    //
+//  Version:                0.3                                             //
 //////////////////////////////////////////////////////////////////////////////
 
 
@@ -28,41 +28,44 @@ typedef struct cell{
 
 
 double output_calculator(double input, double omega, double sigma){
-    return exp( -(((omega-input)/(sigma)) * ((omega-input)/(sigma))) );
+    return exp( -(((omega-input)) * ((omega-input))) / sigma );
 }
 
 
 void calculate_network(CELL *neuron, double *data_feed){
-        double hidden_sum = 0;
-        double output_sum = 0;
+        double hidden_sum_1 = 0;
+        double hidden_sum_2 = 0;
+
         // input layer
-        for(int i=0; i<6; i++){
+        for(int i=0; i<3; i++){
             neuron[i].input  = data_feed[i];
-            neuron[i].output = output_calculator(neuron[i].input, neuron[i].omega, neuron[i].sigma);
-            hidden_sum += neuron[i].output;
+            neuron[i].output = neuron[i].input * neuron[i].omega;
+            hidden_sum_1 += neuron[i].output;
         }
+        for(int i=3; i<6; i++){
+            neuron[i].input  = data_feed[i];
+            neuron[i].output = neuron[i].input * neuron[i].omega;
+            hidden_sum_2 += neuron[i].output;
+        }  
+
         // hidden layer
-        for(int i=6; i<10; i++){
-            neuron[i].input  = hidden_sum;
+        for(int i=6; i<8; i++){
+            neuron[i].input  = hidden_sum_1;
             neuron[i].output = output_calculator(neuron[i].input, neuron[i].omega, neuron[i].sigma);
-            output_sum += neuron[i].output;
         }
-        // output layer
-         for(int i=10; i<12; i++){
-            neuron[i].input  = output_sum;
+        for(int i=8; i<10; i++){
+            neuron[i].input  = hidden_sum_2;
             neuron[i].output = output_calculator(neuron[i].input, neuron[i].omega, neuron[i].sigma);
-        }       
-}
+        }
 
+        // output layer
+        neuron[10].input = neuron[6].output + neuron[8].output;
+        neuron[11].input = neuron[7].output + neuron[9].output;
 
-double calculate_soft_max(CELL *neuron){
-    double sum_1 = 0;
-    double sum_2 = 0;
-    for(int i=0; i<6; i++){
-        sum_1 += neuron[i].activation * neuron[i].output;
-        sum_2 += neuron[i].output;
-    }
-    return sum_1/sum_2;
+        // Output via Softmax
+        neuron[10].output = ((neuron[6].activation*neuron[6].output) + (neuron[8].activation*neuron[8].output)) / (neuron[10].input);
+        neuron[11].output = ((neuron[7].activation*neuron[7].output) + (neuron[9].activation*neuron[9].output)) / (neuron[11].input);
+
 }
 
 
@@ -72,20 +75,28 @@ int main(int argc, char**argv){
     double soft_max;
     CELL neuron[12];
 
-    // defining the input-data
-    double data_feed[6] = {1, 2, 3, 4, 5, 6};
+    // defining the input-data: the input values of the infrared-sensors
+    double data_feed[6] = {8, 0, 0, 0, 0, 0};
 
     // initializing the neurons
     for(int i=0; i<12; i++){
-        neuron[i].activation = i;
-        neuron[i].omega = i+1;
-        neuron[i].sigma = i+2;
+        neuron[i].activation = 1;
+        neuron[i].omega = 1;
+        neuron[i].sigma = 2;
     }
+    neuron[6].activation = -1;
+    neuron[7].activation = 5;
+    neuron[8].activation = 5;
+    neuron[9].activation = -1;
+    neuron[6].omega = 5;
+    neuron[7].omega = 0;
+    neuron[8].omega = 0;
+    neuron[9].omega = 5;
 
     calculate_network(neuron, data_feed);
-    soft_max = calculate_soft_max(neuron);
 
-    printf("Soft Max = %lf\n", soft_max);
+    printf("Left Motor  = %lf\n", neuron[10].output);
+    printf("Right Motor = %lf\n", neuron[11].output);
 
     return 0;
 }
