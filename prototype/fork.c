5bfc32a72c3496f48c443d98c7870471b58956f5
FenFr <HohesCAlt@gmail.com>
2023-06-07 21:35:51 +0200

    Worked on protoype file

--- prototype/fork.c
+++ prototype/fork.c
@@ -12,11 +12,11 @@
 #include <unistd.h>
 
 int main(int argc, char**argv){
-    int module = (int)argv[1][0] - 48;
     if (argc != 2){
         printf("\nType: '%s number'!\n\n", argv[0]);
         exit(1);
     }
+    int module = (int)argv[1][0] - 48;
     if(module < 0 || module > 9 || argv[1][1]){
         printf("\nNumber must be between 0 and 9!\n\n");
         exit(2);
@@ -24,6 +24,7 @@ int main(int argc, char**argv){
 
     int n_of_childs;
     int pid;
+    int pipefd[2];
 
     switch(module){
         case 0:
@@ -34,10 +35,10 @@ int main(int argc, char**argv){
                 printf("Hello, I'm parent.\n");
             break;
 
+
         case 1:
             printf("Type in number of childs: ");
             scanf("%d", &n_of_childs);
-            printf("\n");
 
             while(n_of_childs){
                 pid = fork();
@@ -52,7 +53,24 @@ int main(int argc, char**argv){
                 printf("Hello, I'm parent.\n");
             break;
 
+
+        case 2:
+            printf("Creating pipes:\n");
+            if(pipe(pipefd)){
+                printf("\nError with pipe!\n");
+                exit(3);
+            }
+            if(fork() == 0){
+                printf("Reading (Child) :\n");
+            }
+            else{
+                printf("Writing (Parent):\n");
+            }
+            break;
+
+
         default:
+            printf("Nothing is here ...\n");
             break; 
     }
 
